e2553f2f9c946bbe79d9be0460527df504b5c382
BELLAZIZ <amine.x.bellaziz.-nd@disney.com>
2022-11-24 12:38:13 +0100

    Initial commit

---
+++ src/main/java/com/disney/product/ProductManagerConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.disney.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagerConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagerConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/disney/product/ProductManagerConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disney.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagerConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
